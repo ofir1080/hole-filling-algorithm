67380099af48c19001448277097941dc53cbbc02
Ofir <ofir1080@gmail.com>
2019-04-02 14:48:48 +0300

    Arranged class Image

--- src/main/java/FillingHole/Image.java
+++ src/main/java/FillingHole/Image.java
@@ -23,23 +23,24 @@ public class Image {
     private Type connectivity;
 
 
+    public Image(BufferedImage bImage, Type connectivity) {
 
-    public Image(BufferedImage image) {
-
-        this.width = image.getWidth();
-        this.height = image.getHeight();
+        this.width = bImage.getWidth();
+        this.height = bImage.getHeight();
+        this.connectivity = connectivity;
+        createMatrices(bImage);
         this.pixels = new Point[height][width];
         this.mask = new float[height][width];
 
         // converts RGB to a float in the closed interval [0,1]
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                Color c = new Color(image.getRGB(j, i));
+                Color c = new Color(bImage.getRGB(j, i));
                 int red = (int) (c.getRed() * 0.299);
                 int green = (int) (c.getGreen() * 0.587);
                 int blue = (int) (c.getBlue() * 0.114);
                 float value = (float) (red + green + blue) / 255;
-                Point p = new Point(i ,j, value);
+                Point p = new Point(i, j, value);
                 pixels[i][j] = p;
                 // updates mask
                 if (value > .5) {
@@ -49,7 +50,7 @@ public class Image {
                     mask[i][j] = -1;
                     hole.add(p);
                 }
-                if (isBoundaryHole())
+                findBoundary();
             }
         }
     }
@@ -58,16 +59,42 @@ public class Image {
         return this.mask[y][x] == -1;
     }
 
-    private createMatrices()
+    private void createMatrices(BufferedImage bImage) {
+        // converts RGB to a float in the closed interval [0,1]
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                Color c = new Color(bImage.getRGB(j, i));
 
-    private void createBoundary(Type c) {
+                // converts RGB values  by weight before converting to grayscale (NOTE that weights converge to 1)
+                int red = (int) (c.getRed() * 0.299);
+                int green = (int) (c.getGreen() * 0.587);
+                int blue = (int) (c.getBlue() * 0.114);
+
+                // converts sum to a fraction in the closed interval [0,1]
+                float value = (float) (red + green + blue) / 255;
+                Point p = new Point(i, j, value);
+                pixels[i][j] = p;
+
+                // updates mask
+                if (value > .5) {
+                    mask[i][j] = 0;
+                } else {
+                    // if p is part of the hole, added to the set
+                    mask[i][j] = -1;
+                    hole.add(p);
+                }
+            }
+        }
+    }
+
+    private void findBoundary() {
 
         for (Point p : this.hole) {
             int x = p.getX();
             int y = p.getY();
             for (int i = -1; i <= 1; i += 2) {
                 for (int j = -1; j <= 1; j += 2) {
-                    if (i == j && c == Type.FOUR ) {
+                    if (i == j && this.connectivity == Type.FOUR) {
                         // if FOUR, skip diagonal neighbors
                         continue;
                     }
@@ -78,6 +105,4 @@ public class Image {
             }
         }
     }
-
-    private
 }
